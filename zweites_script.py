66d4b2ddc243c8366810f8444e1407743a2df1a7
JackM <jackm@example.com>
2025-12-04 23:03:28 +0100

    Neue lokale Änderung für Konflikt

--- zweites_script.py
+++ zweites_script.py
@@ -3,8 +3,4 @@ def addiere(a, b):
 
 if __name__ == "__main__":
     ergebnis = addiere(3, 5)
-<<<<<<< HEAD
-    print("Das Ergebnis lautet LOKAL:", ergebnis)
-=======
-    print("Das Ergebnis lautet ONLINE:", ergebnis)
->>>>>>> 96131fab2426e35df0bb7bef6a17bc1494dc83ea
+    print("Ergebnis LOCAL Version:", ergebnis)
